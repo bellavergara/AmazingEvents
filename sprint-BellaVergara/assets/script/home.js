b9f160867c5fcf841398638969365882bf433478
bellavergara <bellaiivanavergara@gmail.com>
2023-08-04 02:46:11 -0300

    proyecto finalizado

--- sprint-BellaVergara/assets/script/home.js
+++ sprint-BellaVergara/assets/script/home.js
@@ -1,28 +1,7 @@
-const contenedorTarjetas = document.getElementById('cards-contenedor')
-
-function crearTarjeta(datosDeEvento) {
-  /*
-    Esta función CREA tarjetas con los datos de cada evento
-  */
-
-    return
-    `<div class="card" style="width: 18rem">
-    <img
-      src="./assets/Recursos_Amazing_Events_Task_1/books.jpg"
-      class="card-img-top"
-      alt=".."
-    />
-    <div class="card-body d-flex flex-column justify-content-between">
-      <h5 class="card-title"></h5>
-      <p crlass="card-text">
-      </p>
-      <div class="d-flex justify-content-between">
-        <p>$</p>
-        <a href="./details.html" class="btn btn-primary"></a>
-      </div>
-    </div>
-  </div>`
-}
+const contenedorTarjetas = document.getElementById('cards-contenedor');
+console.log(data.events);
+const eventos = data.events;
+console.log(eventos);
 
 function mostrarTarjetas(listaDeEventos, contenedorDeEventos){
   /*
@@ -30,41 +9,24 @@ function mostrarTarjetas(listaDeEventos, contenedorDeEventos){
     Crea UNA tarjeta por CADA evento
     Y agrega las tarjetas con los datos de los eventos al contenedor padre
   */
+  let card = "";
   for (const evento of listaDeEventos) {
-    let nuevaTarjeta = crearTarjeta(evento)
-    contenedorDeEventos.innerHTML += nuevaTarjeta
-  }
+    card +=`<div class="card" style="width: 18rem">
+    <img
+      src="${evento.image}"
+      class="card-img-top"
+      alt=".."
+    />
+    <div class="card-body d-flex flex-column justify-content-between">
+      <h5 class="card-title">${evento.name}</h5>
+      <p crlass="card-text">${evento.description}</p>
+      <div class="d-flex justify-content-between">
+        <p>$${evento.price}</p>
+        <a href="./details.html" class="btn btn-primary">details</a>
+      </div>
+    </div>
+  </div>`
 }
-
-
-/// funciones para filtlrar enventos
-
-function filtrarEventosPorNombre(listaDeEventos, nombreEvento) {
-  let eventosFiltrados
-  for (const evento of listaDeEventos) {
-    if(evento.name == nombreEvento) {
-      eventosFiltrados.push(evento)
-    }
-  }
-  return eventosFiltrados
+  contenedorDeEventos.innerHTML = card;
 }
-
-function filtrarEventosPorCategoria(listaDeEventos, categoria) {
-  let eventosFiltrados
-  for (const evento of listaDeEventos) {
-    if(evento.category == categoria) {
-      eventosFiltrados.push(evento)
-    }
-  }
-  return eventosFiltrados
-}
-
-function filtrarEventosPorLugar(listaDeEventos, lugar) {
-  let eventosFiltrados
-  for (const evento of listaDeEventos) {
-    if(evento.place == lugar) {
-      eventosFiltrados.push(evento)
-    }
-  }
-  return eventosFiltrados
-}
+mostrarTarjetas(eventos,contenedorTarjetas);
---
+++ sprint-BellaVergara/assets/script/pastEvents.js
@@ -0,0 +1,52 @@
+const contenedorTarjetas = document.getElementById('contenedor-pasEvents');
+console.log(data.events);
+const eventos = data.events;
+console.log(eventos);
+
+
+function filtradoDato(listaTarjeta){
+    let datosFiltrados =[];
+    for (const datos of listaTarjeta) {
+    if(datos.date < data.currentDate ){
+        datosFiltrados.push(datos)
+      }
+    }
+    return datosFiltrados
+}
+// aca estoy bajando la funcion y guardando el contenido en una variable
+// para luego  llamarla en la funcion (mostrar tarjeta)//
+
+const datosFiltrados = filtradoDato(eventos)
+
+function mostrarTarjetas(listaDeEventos, contenedorDeEventos){
+  /*
+    Esta función recibe la lista de eventos del archivo "dataAmazingEvents.js"
+    Crea UNA tarjeta por CADA evento
+    Y agrega las tarjetas con los datos de los eventos al contenedor padre
+  */
+  let card = "";
+  for (const evento of listaDeEventos) {
+    card +=`<div class="card" style="width: 18rem">
+    <img
+      src="${evento.image}"
+      class="card-img-top"
+      alt=".."
+    />
+    <div class="card-body d-flex flex-column justify-content-between">
+      <h5 class="card-title">${evento.name}</h5>
+      <p crlass="card-text">${evento.description}</p>
+      <div class="d-flex justify-content-between">
+        <p>$${evento.price}</p>
+        <a href="./details.html" class="btn btn-primary">details</a>
+      </div>
+    </div>
+  </div>`
+}
+  contenedorDeEventos.innerHTML = card;
+  
+}
+mostrarTarjetas(datosFiltrados,contenedorTarjetas);
+
+
+
+
